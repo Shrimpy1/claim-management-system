aa6882ec44423af13c288c5fc93098a393389ce3
Shrimpy <s3978724@rmit.edu.vn>
2024-04-25 03:08:46 +0700

    initialize

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module rmit.furtherprog.claimmanagementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens rmit.furtherprog.claimmanagementsystem to javafx.fxml;
+    exports rmit.furtherprog.claimmanagementsystem;
+}
